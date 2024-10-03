96f914905e61e69d2b3877dcb5410953b0f0a1b2
hurara12 <hurara.waseem@gmail.com>
2024-10-03 11:24:41 +0500

    updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,27 +9,27 @@ import Profile from './pages/generals/Profile';
 import ViewCatalogue from './components/profile/ViewCatalogue';
 import { Toaster } from './shadcn-components/ui/toaster';
 import ViewCatalogueSm from './components/profile/ViewCatalogueSm';
-import Search  from './pages/generals/Search';
+import Search from './pages/generals/Search';
 import Spaces from './pages/generals/Spaces';
 import SpaceDashboard from './components/spaces/SpaceDashboard';
-
-const App=()=> {
+//eee
+const App = () => {
   return (
     <BrowserRouter>
-    <AuthProvider>
-      <Routes>
-        <Route path="/" element={<Auth/>} />
-        <Route path="/dashboard" element={<GeneralProtectedRoute elementBody={<Dashboard />} />} />
-        <Route path="/profile/:userId" element={<GeneralProtectedRoute elementBody={<Profile />} />} />
-        <Route path="/view-catalogue-sm" element={<GeneralProtectedRoute elementBody={<ViewCatalogueSm />} />} />
-        <Route path="/search" element={<GeneralProtectedRoute elementBody={<Search/>}/>}/>
-        <Route path="/spaces" element={<GeneralProtectedRoute elementBody={<Spaces/>}/>}/>
-        <Route path="/space-dashboard/:spaceName" element={<GeneralProtectedRoute elementBody={<SpaceDashboard/>}/>}/>
-        <Route path="/test" element={<ViewCatalogue/>}/>
-        
-      </Routes>
-      <Toaster/>
-    </AuthProvider>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Auth />} />
+          <Route path="/dashboard" element={<GeneralProtectedRoute elementBody={<Dashboard />} />} />
+          <Route path="/profile/:userId" element={<GeneralProtectedRoute elementBody={<Profile />} />} />
+          <Route path="/view-catalogue-sm" element={<GeneralProtectedRoute elementBody={<ViewCatalogueSm />} />} />
+          <Route path="/search" element={<GeneralProtectedRoute elementBody={<Search />} />} />
+          <Route path="/spaces" element={<GeneralProtectedRoute elementBody={<Spaces />} />} />
+          <Route path="/space-dashboard/:spaceName" element={<GeneralProtectedRoute elementBody={<SpaceDashboard />} />} />
+          <Route path="/test" element={<ViewCatalogue />} />
+
+        </Routes>
+        <Toaster />
+      </AuthProvider>
     </BrowserRouter>
   )
 }
